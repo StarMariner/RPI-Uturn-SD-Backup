ab0573d5f4986d8e820c8cdff293f6a6c20ea511
starmariner <70407810+StarMariner@users.noreply.github.com>
2020-11-19 18:51:53 +0000

    Add files via upload
    Bug fix , better BASH directory test and create. More efficient.

--- RPI-Uturn-SD-Backup.bat
+++ RPI-Uturn-SD-Backup.bat
@@ -19,9 +19,8 @@ REM [X] DONE. WIP, Need to find a better way of detecting if you are copying to 
 REM NOTE: RaspberryPiOS is new, it replaces what was previously known as Raspbian
 REM Prerequisites. None needed as of October 2020 . Windows 10, SSH is now built in. RaspberryPiOS, ntfs /cifs now builtin.
 REM Sort ssh timeout. May be ping or something? NSlookup is good but it doesnt show if its live or dead.
-
 :BEGIN
-setlocal enabledelayedexpansion 
+
 
 
 
@@ -30,20 +29,21 @@ REM [[[[[[[[[[[[[ CHANGE SET VALUES TO MATCH YOUR DEVICES ]]]]]]]]]]]
 REM [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 
 REM Replace windows shared folder name below to match yours. Manually remove any leftside characters before //. eg. SET win_shared=//machine/shared_folder
-SET win_shared=//ExampleMachineName/Shared_Folder
-SET win_user=ExampleUser
+SET win_shared=//MACHINE/pi_BKUP_FOLDER
+SET win_user=USERNAME
+
 REM mountpoint on pi 
 SET pi_winshare=~/winshare
 
 REM Add as many Pis as you like , but make sure LastPi=x value is the last UserPI[x] and last ipPI[x]
 SET UserPI[1]=pi
-SET ipPI[1]=192.168.1.24
+SET   ipPI[1]=192.168.1.100
 
 SET UserPI[2]=pi
-SET ipPI[2]=192.168.1.110
+SET   ipPI[2]=192.168.1.110
 
 SET UserPI[3]=pi
-SET ipPI[3]=192.168.1.112
+SET   ipPI[3]=192.168.1.120
 
 SET LastPI=3
 
@@ -61,7 +61,7 @@ ECHO/
 ECHO INSTRUCTIONS
 ECHO *. You must enable Windows sharing for the Destination Folder. 
 ECHO *. Edit this BAT file. Change SET values to match your devices.
-ECHO *. Select a device [x] to backup
+ECHO *. Select a device [x] to backup.
 ECHO *. Check Source and Destination. If there are errors check this BAT file.
 ECHO *. Enter the passwords for the Source and Destination.
 ECHO *. Wait for the automated script to connect to the source. It then creates a disk image on the destination.
@@ -70,16 +70,17 @@ IF NOT EXIST %win_shared% ECHO *** STOP. =FIX THIS BROKEN PATH  *** SET win_shar
 
 ECHO/
 ECHO No.  User @ IP address    NS Lookup Name
+setlocal enabledelayedexpansion 
 for /l %%n in (1,1,%LastPi%) do ( 
-			SET sshPI[%%n]=!UserPI[%%n]!@!ipPI[%%n]!
-			SET NiceNamePI[%%n]=Not_Available
-			FOR /f "tokens=2" %%i IN ('nslookup !ipPI[%%n]! 2^>nul ^| findstr /C:"Name"') DO SET NiceNamePI[%%n]=%%i
-			ECHO [%%n]  !sshPI[%%n]!    !NiceNamePI[%%n]!
-			)
+								SET NiceNamePI[%%n]=Not_Available
+								FOR /f "tokens=2" %%i IN ('nslookup !ipPI[%%n]! 2^>nul ^| findstr /C:"Name"') DO SET NiceNamePI[%%n]=%%i
+								ECHO [%%n]  !UserPI[%%n]!@!ipPI[%%n]!    !NiceNamePI[%%n]!
+								)
 ECHO/
 
-
 :PickPi
+REM SET /P input exploit: REF: https://www.robvanderwoude.com/battech_inputvalidation_setp.php
+REM I am checking for numbers here, exploits should not happen. So, not using the bug fix  !var! | FIND """" >NUL && SET var=  
 SET /P GetInput= Choose a device number [?] to backup.  
 IF %GetInput% LEQ 0 GOTO BEGIN
 IF %GetInput% LEQ %LastPi% GOTO BKUP_SCRIPT
@@ -89,28 +90,29 @@ GOTO END
 
 :BKUP_SCRIPT
 IF !NiceNamePI[%getInput%]!==Not_Available ( ECHO That Device is NOT AVAILABLE choose another.
-  				          GOTO PickPi )											  
-SET sshPI[%getInput%]=!UserPI[%getInput%]!@!ipPI[%getInput%]!
-SET nicefilename=!NiceNamePI[%GetInput%]!.img
-
+											 GOTO PickPi )	
+SET NiceFileNamePI=!NiceNamePI[%GetInput%]!.img
 ECHO/
 ECHO =============== Backup Remote DISK to Local IMAGE file =================
 ECHO | SET /p dummy = Local Machine\User = 
 whoami
-ECHO DISK SOURCE:      !sshPI[%getInput%]!  
-ECHO IMG DESTINATION:  %win_shared%/%nicefilename% 
+ECHO DISK SOURCE:      !UserPI[%GetInput%]!@!ipPI[%GetInput%]!
+ECHO IMG DESTINATION:  %win_shared%/%NiceFileNamePI%
 
 ECHO/
+REM description of [ ! -d %pi_winshare% ] see BASH book P47
+REM [ ] TEST an expression within the square beackets.
+REM  ! true if expression is false , if NOT. 
+REM  -dw  is file a directory ? return true if exists. -w file True if file exists and is writable. BASH conditional expression p90
 REM  The following linux commands inside the Double quotes can be put into a shell script and run locally on the pi.
 REM *******S the Process still running on the pi when I log off on Windows? Do I need to KILL a process? it does
 REM SEE fuser command to stop bust resources.NEED CODE FOR PROCESS START AND KILL 
+REM TODO  check that an existing file isnt there, if so rename it as .bkup or delete it if its broken. date +%k%M%H%h%Y
 
-SET piMountBkupScript=" [ ! -d  %pi_winshare% ] && mkdir %pi_winshare% ; sudo mount.cifs %win_shared% %pi_winshare% -o user=%win_user% ; ls -l %pi_winshare% ; echo ; echo START of backup file: %nicefilename%, please wait ; sudo -S dd bs=4M if=/dev/mmcblk0 status=progress of=%pi_winshare%/%nicefilename% ; echo COMPLETED transfer ; sudo umount %pi_winshare% ;  cd ~ ; bash "
+SET piMountBkupScript=" mkdir -p %pi_winshare% ; sudo mount.cifs %win_shared% %pi_winshare% -o user=%win_user% ; ls -l %pi_winshare% ; echo ; echo START of backup file: !NiceFileNamePi!, please wait ; sudo -S dd bs=4M if=/dev/mmcblk0 status=progress of=%pi_winshare%/!NiceFileNamePi! ; echo COMPLETED transfer ; sudo umount %pi_winshare% ; cd ~ ; bash "
 
-ssh -t !sshPI[%getInput%]!  %piMountBkupScript%
-
-
-GOTO BEGIN
+REM example ssh -t pi@raspberry piMountBkupScript
+ssh -t  !UserPI[%getInput%]!@!ipPI[%getInput%]! %piMountBkupScript%
 
 :END
 ECHO  Finished .
